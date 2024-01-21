b805e4d129dd1d2b4fc459c3d42b64e8ddf35753
Rafael Gomez <rafaelogomezr@gmail.com>
2024-01-21 17:39:40 -0400

    serializando un objeto a json con JsonSerializer.Serializer(nombre del objeto), recuerda que debes importar using System.Text.Json;

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,9 +1,20 @@
 ﻿namespace ATM;
-
+using System.Text.Json;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var usuario = new Usuario
+            {
+                Nombre="rafael gomez",
+                Cedula="121ds",
+                Correo="ds@gmail.com",
+                Telefono = 806954
+            };
+
+            string jsonString = JsonSerializer.Serialize(usuario);
+
+            Console.WriteLine(jsonString);
     }
 }
---
+++ Main/Usuario/Usuario.cs
@@ -0,0 +1,7 @@
+public class Usuario
+{
+    public string? Nombre { get; set; }
+    public string? Cedula { get; set; }
+    public string? Correo { get; set; }
+    public int Telefono { get; set; }
+}
