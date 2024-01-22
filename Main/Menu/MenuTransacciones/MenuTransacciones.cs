9a0bbd07629178ee74edb49f11ee8817bfe8b490
Rafael Gomez <rafaelogomezr@gmail.com>
2024-01-21 20:15:32 -0400

    funciones al menuTransacciones

--- Main/Menu/MenuTransacciones/MenuTransacciones.cs
+++ Main/Menu/MenuTransacciones/MenuTransacciones.cs
@@ -1,6 +1,10 @@
 public class MenuTransacciones{
     public static void MenuTransacciones()
     {
+        while (true)
+        {
+            
+        
         System.Console.WriteLine(@"
         Que operacion deseas realizar?
         
@@ -15,11 +19,41 @@ public class MenuTransacciones{
 
         opcion=int.Parse(Console.ReadLine());
 
-        switch (opcion)
-        {case
-            case
-            
+        switch ((OperacionCajero)opcion)
+        {  
+            case OperacionCajero.Depositar:
+                MenuDepositar.MenuDepositar();
+                break;
+            case OperacionCajero.Retirar:
+                MenuRetirar.MenuRetirar();
+                break;
+            case OperacionCajero.ConsultarBalance:
+                MenuConsultarBalance.MenuConsultarBalance();
+                break;
+            case OperacionCajero.Retirar:
+                MenuRetirar.MenuRetirar();
+                break;
+            case OperacionCajero.Retirar:
+                MenuRetirar.MenuRetirar();
+                break;
+            case OperacionCajero.Retirar:
+                MenuRetirar.MenuRetirar();
+                break;
+        
             default:
+                System.Console.WriteLine("Haz introducido un valor incorrecto.");
+                continue;
         }
+        }
+        
     }
+    public enum OperacionCajero
+        {
+            Depositar,
+            Retirar,
+            ConsultarBalance,
+            PagarPrestamo,
+            PagarTarjeta,
+            Salir
+        }
 }
