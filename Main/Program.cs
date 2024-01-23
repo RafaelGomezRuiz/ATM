171f862e53fe69886e303454640b4655d2402d31
Rafael Gomez <rafaelogomezr@gmail.com>
2024-01-23 13:11:28 -0400

    Todo funciona, cambie los archivos json y usuarios

--- Main/Program.cs
+++ Main/Program.cs
@@ -6,8 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {   
-        MenuPrincipal.ShowMenuCredenciales();
-        //Codigo para guardar los usuarios
+        //MenuPrincipal.ShowMenuCredenciales();
         // List<Usuario> ResgisUsuario=new List<Usuario>{
         //     new Usuario{
         //         Nombre="rafael gomez",
@@ -20,26 +19,38 @@ class Program
         //         Cedula="4022000",
         //         Correo="Aldedilloz@gmail.com",
         //         Telefono = 806803
+        //     },
+        //         new Usuario{
+        //         Nombre="Oliver Betances",
+        //         Cedula="4022000",
+        //         Correo="olioli@gmail.com",
+        //         Telefono = 806803
+        //     },
+        //         new Usuario{
+        //         Nombre="El negrito ojos claros",
+        //         Cedula="4022000",
+        //         Correo="Ozhuna@gmail.com",
+        //         Telefono = 806803
         //     }
         // };
 
         // GuardarJson.GuardarUsuarios(ResgisUsuario);
 
-            //Este es el ejmplo de
-            // string jsonString = JsonSerializer.Serialize(usuario);
-            // Console.WriteLine(jsonString);
+        //    // Este es el ejmplo de
+        //     //string jsonString = JsonSerializer.Serialize(usuario);
+        //    // Console.WriteLine(jsonString);
             
-            // List<Usuario> usuarios=LeerJson.LeerUsuario();
+        //     List<Usuario> usuarios=LeerJson.LeerUsuario();
 
-            // List<Cuenta> CuentaUser=new List<Cuenta>();
+        //     List<Cuenta> CuentaUser=new List<Cuenta>();
 
-            // foreach (var cuenta1 in usuarios)
-            // {
-            //    Cuenta cuentas = new Cuenta(cuenta1);
-            //    cuentas.NumeroCuenta+=1;
-            //     CuentaUser.Add(cuentas);
-            // }
+        //     foreach (var cuenta1 in usuarios)
+        //     {
+        //        Cuenta cuentas = new Cuenta(cuenta1);
+        //        cuentas.NumeroCuenta+=1;
+        //         CuentaUser.Add(cuentas);
+        //     }
             
-            // GuardarJson.GuardarCuenta(CuentaUser);
+        //     GuardarJson.GuardarCuenta(CuentaUser);
     }
 }
