1b0a0e8286d041a4332bcad40e14f7a7642202b2
Rafael Gomez <rafaelogomezr@gmail.com>
2024-01-28 19:21:25 -0400

    traje el json de usuarios de otro commit

--- Main/obj/Debug/net8.0/ATM.AssemblyInfo.cs
+++ Main/obj/Debug/net8.0/ATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f0c23e5aef653c43cf9e67457f0beeb2cb10a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011b1342fb380f36b202e66f8169dbbb1ecac378")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
